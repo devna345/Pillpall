740d064c587dff8a6e4aea5c9a72457499af6b75
glennis <glennissebastian17@gmail.com>
2026-03-01 22:38:48 +0530

    HI

--- app.py
+++ app.py
@@ -38,6 +38,10 @@ def second_reminder():
     print("⚠️ Did you take your medicine? Please confirm!")
 
 # Home Page
+@app.route('/')
+def home():
+        return render_template('home.html')
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     med = Medicine.query.first()
@@ -64,9 +68,9 @@ def index():
             minute=minute
         )
 
-        return redirect(url_for('index'))
+        return redirect(url_for('details'))
 
-    return render_template('index.html', med=med)
+    return render_template('details.html', med=med)
 
 # Mark as taken
 @app.route('/taken')
@@ -88,5 +92,5 @@ def progress():
     med = Medicine.query.first()
     return render_template('progress.html', med=med)
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True)
